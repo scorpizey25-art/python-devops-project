48365363217cf2df210c869dbebb6ada6d997e78
scorpizey25-art <scorpizey25@gmail.com>
2025-08-25 14:45:55 +0700

    menambahkan file class server baru

---
+++ cek_statusserver.py
@@ -0,0 +1,30 @@
+STATUS_AKTIF = "aktif"
+STATUS_NON_AKTIF = "non-aktif"
+
+
+class Server:
+    def __init__(self, hostname, ipaddress, status=STATUS_AKTIF):
+        self.hostname = hostname
+        self.ipaddress = ipaddress
+        self.status = status
+
+    def cek_status(self):
+        if self.status == STATUS_AKTIF:
+            return "Kondisi Server Aktif"
+        else:
+            return ValueError("Status server tidak valid")
+
+    def set_status(self, new_status):
+        self.status = new_status
+        print(f"Status server {self.hostname} diubah menjadi {self.status}")
+
+
+def kelolahServer():
+    server_web = Server("web-server", "192.168.8.110")
+    print(server_web.cek_status())
+
+    print("Mengubah status server...")
+    server_web.set_status(STATUS_AKTIF)
+
+
+kelolahServer()
